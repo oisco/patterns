8572b9a20f560b42d16e7c67fd67a8ffeb7060d6
Oisin <ois.cfc@hotmail.com>
2017-03-02 00:32:21 +1300

    extension of showDialog abstract class, leverages off all code to modify a record from EmployeeDetails class

---
+++ src/main/EditRecordDialog.java
@@ -0,0 +1,67 @@
+package main;
+
+import javax.swing.*;
+import java.io.File;
+
+/**
+ * Created by Oisín on 3/1/2017.
+ */
+public class EditRecordDialog extends RecordDialog{
+    Employee currentEmployee;
+
+    public EditRecordDialog(EmployeeDetails parent, String title, File file, long currentByteStart,Employee currentEmployee,RandomFile application) {
+        super(parent, title,currentByteStart,file,application);
+        this.currentEmployee=currentEmployee;
+
+        setUpFields();
+        getRootPane().setDefaultButton(save);
+        setSize(500, 370);
+        setLocation(350, 250);
+        setVisible(true);
+
+    }
+
+    @Override
+    public void addOrUpdate() {
+            int returnVal = JOptionPane.showOptionDialog(this, "Do you want to save changes to current Employee?", "Save",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
+            // if user choose to save changes, save changes
+            if (returnVal == JOptionPane.YES_OPTION) {
+                // open file for writing
+                application.openWriteFile(this.parent.file.getAbsolutePath());
+                // get changes for current Employee
+                currentEmployee = getChangedDetails();
+                // write changes to file for corresponding Employee record
+                application.changeRecords(currentEmployee, currentByteStart);
+                application.closeWriteFile();// close file for writing
+                this.parent.displayRecords(currentEmployee);
+                dispose();// dispose dialog
+//                changesMade = false;// state that all changes has bee saved
+            } // end if
+            setEnabled(false);
+        }// end saveChanges
+
+    private Employee getChangedDetails() {
+        Employee theEmployee;
+        if (((String) fullTimeCombo2.getSelectedItem()).equalsIgnoreCase("Yes")){
+            theEmployee = getFromFields();
+            return theEmployee;
+        }
+        else{
+            return null;
+        }
+    }// end getChangedDetails
+
+    public void setUpFields(){
+        //NB --update combos
+        idField2.setText(String.valueOf(this.currentEmployee.getEmployeeId()));
+        firstNameField2.setText(this.currentEmployee.getFirstName().trim());
+        surnameField2.setText(this.currentEmployee.getSurname().trim());
+        salaryField2.setText(String.valueOf(this.currentEmployee.getSalary()).trim());
+        ppsField2.setText(this.currentEmployee.getPps().trim());
+        departmentCombo2.setSelectedIndex(1);////come back to chenge dept and gender to be numeric
+        genderCombo2.setSelectedIndex(2);
+        fullTimeCombo2.setSelectedIndex(2);
+    }
+
+}
